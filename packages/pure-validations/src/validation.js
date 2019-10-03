eec53fab1141f36af7802a2d2c3210a829411587
Radu Popovici <rpopovici@totalsoft.ro>
2019-10-03 15:07:19 +0300

    added arcadia proj

--- packages/pure-validations/src/validation.js
+++ packages/pure-validations/src/validation.js
@@ -9,20 +9,20 @@ const Success = Maybe.Nothing;
 const Failure = Maybe.Just;
 
 function isValid(validation) {
-    return Maybe.Nothing.is(validation);
-  }
-  
-  function getErrors(validation) {
-    const maybeArray = validation |> chain(RoseTree.getValue) |> map(List.toArray);
-    const result = maybeArray.cata({
-      Just: id,
-      Nothing: constant([])
-    });
-    return result;
-  }
-  
-  const getInner = curry(function getInner(path, validation) {
-    return path.reduce((acc, key) => chain(ValidationError.getField(key), acc), validation);
+  return Maybe.Nothing.is(validation);
+}
+
+function getErrors(validation) {
+  const maybeArray = validation |> chain(RoseTree.getValue) |> map(List.toArray);
+  const result = maybeArray.cata({
+    Just: id,
+    Nothing: constant([])
   });
+  return result;
+}
+
+const getInner = curry(function getInner(path, validation) {
+  return path.reduce((acc, key) => chain(ValidationError.getField(key), acc), validation);
+});
 
 export { Success, Failure, isValid, getErrors, getInner };
