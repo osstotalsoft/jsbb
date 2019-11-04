9acf14d951c4fc379ebb5075f25e1a921811d177
fraliv13 <5892139+fraliv13@users.noreply.github.com>
2019-11-04 10:37:06 +0200

    #17 Validation proxy fails for field name "value" (#18)

---
+++ packages/pure-validations-react/src/validationProxy/__tests__/validationProxy.tests.js
@@ -0,0 +1,30 @@
+import { ValidationProxy, eject } from "../";
+import { Success, Failure, ValidationError } from "@totalsoft/pure-validations";
+
+
+jest.unmock("@totalsoft/pure-validations");
+
+describe("validation proxy", () => {
+    it("returns inner validation", () => {
+        // Arrange
+        const validation = Failure(ValidationError("", { value: ValidationError("Error") }))
+        const proxy = ValidationProxy(validation)
+
+        // Act
+        const innerValidation = eject(proxy.value);
+
+        // Assert
+        expect(innerValidation).toStrictEqual(Failure(ValidationError("Error")))
+    });
+    it("returns success if inner validation not found", () => {
+        // Arrange
+        const validation = Success
+        const proxy = ValidationProxy(validation)
+
+        // Act
+        const innerValidation = eject(proxy.value);
+
+        // Assert
+        expect(innerValidation).toBe(Success)
+    });
+})
