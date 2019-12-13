2a12065c66efe0cbddb634f79f05391540200e7b
fraliv13 <5892139+fraliv13@users.noreply.github.com>
2019-12-13 14:28:30 +0200

    moved rule concateation to chainRules

--- packages/pure-rules/src/rule.js
+++ packages/pure-rules/src/rule.js
@@ -1,19 +1,9 @@
 import Reader from "@totalsoft/zion/data/reader";
-import { curry, chain} from "ramda";
+import { curry } from "ramda";
 import fl from "fantasy-land";
-import { contramap } from "@totalsoft/zion";
-import { setInnerProp } from "./objectUtils";
 
 export const Rule = Reader;
 Rule.of = Rule[fl.of];
-Rule.prototype[fl.concat] = function (that) {
-    return this |> chain(newModel => that |> contramap((model, ctx) => [newModel, _getContext(model, newModel, ctx)]))
-};
-
-function _getContext(model, newModel, ctx) {
-    const { fieldPath, document } = ctx;
-    return model === newModel ? ctx : { ...ctx, document: setInnerProp(document, fieldPath, newModel) }
-}
 
 const emptyContext = {
     prevDocument: undefined,
