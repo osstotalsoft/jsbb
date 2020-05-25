5537f6570daec1ecc49fbd27667ba48d5a4ea48b
fraliv13 <5892139+fraliv13@users.noreply.github.com>
2020-05-25 11:52:17 +0300

    Fix null model value in fromModel HoV

--- packages/pure-validations/src/higherOrderValidators/fromModel.js
+++ packages/pure-validations/src/higherOrderValidators/fromModel.js
@@ -1,16 +1,11 @@
 import Reader from "@totalsoft/zion/data/reader";
-import Maybe from "@totalsoft/zion/data/maybe";
 import { $do } from "@totalsoft/zion";
 import { checkValidators } from "./_utils";
-
-const {Nothing} = Maybe;
 
 export default function fromModel(validatorFactory) {
   return $do(function*() {
     const [model] = yield Reader.ask();
-    if (model === null || model === undefined) {
-      return Nothing;
-    }
+
     const v = validatorFactory(model);
     checkValidators(v);
     return yield v;
