22caf29f92d73ed2627dd3ac8b950b1b719926af
fraliv13 <5892139+fraliv13@users.noreply.github.com>
2021-06-03 18:22:44 +0300

    fix array immutable assignement

--- packages/rules-algebra/src/objectUtils/index.js
+++ packages/rules-algebra/src/objectUtils/index.js
@@ -29,6 +29,6 @@ function _immutableAssign(obj, prop, value) {
     }
 
     return Array.isArray(obj)
-        ? Object.assign([...obj], { [prop]: value })
+        ? Object.assign([], obj, { [prop]: value })
         : { ...obj, [prop]: value }
 }
